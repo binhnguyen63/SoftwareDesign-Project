cd8609f4d964f87816e049d3a39c4b5e16ea5bc0
Binh Nguyen <bennguyen1490@gmail.com>
2025-03-15 20:01:28 -0500

    fixed app_config

--- app_config.py
+++ app_config.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-load_dotenv()
+load_dotenv(override=True)
 # Application (client) ID of app registration
 CLIENT_ID = os.getenv("CLIENT_ID")
 print("here",CLIENT_ID)
@@ -32,6 +32,7 @@ ADMIN = os.getenv("ADMIN")
 # it's better to use a database-backed session store instead.
 DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
+print(DB_PORT)
 DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
