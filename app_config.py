35dbe7e7e945a36a15c07903734d573ad00fad75
Binh Nguyen <bennguyen1490@gmail.com>
2025-04-02 13:34:43 -0500

    fixed default port

--- app_config.py
+++ app_config.py
@@ -30,7 +30,7 @@ SESSION_TYPE = "filesystem"
 ADMIN = os.getenv("ADMIN")
 
 DB_HOST = os.getenv("DB_HOST","db")
-DB_PORT = os.getenv("DB_PORT","5432")
+DB_PORT = os.getenv("DB_PORT","5433")
 DB_NAME = os.getenv("DB_NAME","postgres")
 DB_USER = os.getenv("DB_USER","postgres")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "ben123")
